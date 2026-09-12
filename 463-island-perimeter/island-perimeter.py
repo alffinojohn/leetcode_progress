3cedd4ecfc59058ac28c449268faeea05f484945
alffinojohn <109807983+alffinojohn@users.noreply.github.com>
2026-09-12 18:35:27 -0400

    Time: 120 ms (23.78%) | Memory: 35.2 MB (9.36%) - LeetSync

---
+++ 463-island-perimeter/island-perimeter.py
@@ -0,0 +1,33 @@
+class Solution:
+    def islandPerimeter(self, grid: List[List[int]]) -> int:
+        row = len(grid)
+        col = len(grid[0])
+        visited = set()
+
+        def dfs(r, c):
+            # outside
+            if(
+                r < 0 or r >= row or c < 0 or c >= col
+            ):
+                return 1
+            
+            # water
+            if (grid[r][c] == 0):
+                return 1
+
+            if (r,c) in visited:
+                return 0
+
+            visited.add((r,c))
+
+            return(
+                dfs(r+1, c) +
+                dfs(r-1,c) +
+                dfs(r,c+1) +
+                dfs(r,c-1)
+            )
+
+        for r in range(row):
+            for c in range(col):
+                if grid[r][c] == 1:
+                    return dfs(r,c)
