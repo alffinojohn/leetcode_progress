5803792ba3d3693cfbf4d1f3ec4dab2690fcbfd7
alffinojohn <109807983+alffinojohn@users.noreply.github.com>
2026-09-13 23:11:47 -0400

    Time: 0 ms (100.00%) | Memory: 20.2 MB (7.43%) - LeetSync

--- 733-flood-fill/flood-fill.py
+++ 733-flood-fill/flood-fill.py
@@ -7,20 +7,18 @@ class Solution:
         if old == color:
             return image
 
-        def dfs(r,c):
-            if (
+        def dfs(r, c):
+            if(
                 r < 0 or r >= row or c < 0 or c >= col or image[r][c] != old
             ):
                 return
 
             image[r][c] = color
 
-            dfs(r+1, c)
-            dfs(r-1, c)
+            dfs(r+1,c)
+            dfs(r-1,c)
             dfs(r,c+1)
             dfs(r,c-1)
 
-        dfs(sr, sc)
+        dfs(sr,sc)
         return image
-
-        
