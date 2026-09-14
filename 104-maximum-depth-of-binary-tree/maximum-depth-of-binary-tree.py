3abb2d200dbfe520624d675bea88c13c883098a2
alffinojohn <109807983+alffinojohn@users.noreply.github.com>
2026-09-13 23:40:06 -0400

    Time: 3 ms (48.53%) | Memory: 22.4 MB (71.67%) - LeetSync

--- 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.py
+++ 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.py
@@ -6,15 +6,15 @@
 #         self.right = right
 class Solution:
     def maxDepth(self, root: Optional[TreeNode]) -> int:
+
         def dfs(node):
             if not node:
                 return 0
-            
+
             left = dfs(node.left)
             right = dfs(node.right)
 
-            return max(left, right) + 1
+            return max(left,right) + 1
 
         return dfs(root)
-
         
